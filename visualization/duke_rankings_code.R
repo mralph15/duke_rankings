1c1f5906e823425572a646463a31a9287bae38ab
Matthew Ralph <mr366@duke.edu>
2021-10-02 06:57:15 -0400

    Created animated line-plot of rank since 2000.

--- visualization/duke_rankings_code.R
+++ visualization/duke_rankings_code.R
@@ -2,6 +2,7 @@
 
 #Load required packages
 library(tidyverse)
+library(gganimate)
 
 #Load data
 duke <- read.csv("~/Documents/R Files/duke_rankings/data/duke_rankings.csv")
@@ -24,4 +25,53 @@ duke %>%
         legend.position = c(0.1, 0.8),
         legend.title = element_text(size = 14),
         legend.text = element_text(size = 10))
+
+#Creating static plot of ranks from 2000 onward
+duke %>%
+  ggplot(aes(x = YEAR, y = RANK, color = ORG)) +
+  geom_line() +
+  geom_point() + 
+  geom_hline(yintercept = 10, linetype = "dashed", color = "grey60") + 
+  scale_y_continuous(breaks = c(0, 10, 25, 50, 75, 100),
+                     minor_breaks = c(12.5, 37.5, 62.5, 87.5)) +
+  scale_color_manual(values = c("#9ecae1", "#4292c6", "#08306b")) + 
+  coord_cartesian(xlim = c(2000,2022), ylim = c(0, 110)) +
+  labs(title = "Duke University College Rankings",
+       subtitle = "since 2000",
+       x = "Year",
+       y = "Rank",
+       color = "Organization") + 
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 16),
+        plot.subtitle = element_text(hjust = 0.5, size = 12),
+        legend.position = c(0.1, 0.8),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 10))
+
+#Creating moving plot of ranks from 2000 onward
+moving_plot <- duke %>%
+  filter(YEAR >= 2000) %>%
+  ggplot(aes(x = YEAR, y = RANK, color = ORG)) +
+  geom_line() +
+  geom_point() + 
+  geom_hline(yintercept = 10, linetype = "dashed", color = "grey60") + 
+  scale_y_continuous(breaks = c(0, 10, 25, 50, 75, 100),
+                     minor_breaks = c(12.5, 37.5, 62.5, 87.5)) +
+  scale_color_manual(values = c("#9ecae1", "#4292c6", "#08306b")) + 
+  transition_reveal(YEAR) +
+  labs(title = "Duke University College Rankings",
+       subtitle = "since 2000",
+       x = "Year",
+       y = "Rank",
+       color = "Organization") + 
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5, size = 16),
+        plot.subtitle = element_text(hjust = 0.5, size = 16),
+        legend.position = c(0.1, 0.8),
+        legend.title = element_text(size = 14),
+        legend.text = element_text(size = 10))
+
+animate(moving_plot, end_pause = 30)
+
+anim_save("duke_rankings_since_2000.gif")
 
